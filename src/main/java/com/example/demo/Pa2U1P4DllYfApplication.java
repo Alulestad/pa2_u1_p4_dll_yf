e446a199c576d5e178f63769f845d20633c930d4
alulestad <vladimirlestad@gmail.com>
2023-05-15 19:35:49 -0500

    taller4

--- src/main/java/com/example/demo/Pa2U1P4DllYfApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4DllYfApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,6 +26,17 @@ public class Pa2U1P4DllYfApplication implements CommandLineRunner{
 		 * Spring framework: Framework que permite la construcción/desarrollo de 
 		 * aplicaciones empresariales basadas en spring
 		 */
+		
+		Profesor profe=new Profesor();
+		profe.setApellido("Llumiquinga");
+		profe.setCedula("1721495628");
+		profe.setFechaNacimiento(LocalDateTime.now());
+		profe.setNombre("Daniel");
+		
+		
+		System.out.println(profe);
+		
+		
 		
 	}
 
